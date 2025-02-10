dcff1046160a9fd21ae97aaa8a4b5cf8d863e2b8
abdulaziz2706 <abdulaziz27707@gmail.com>
2025-02-10 12:44:59 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+let str = prompt('String turidagi malumot kiriting');
+let bool = prompt('Boolean turidagi malumot kiriting');
+let num = prompt('Number turidagi malumot kiriting');
+
+alert('Malumotlarni togri kiritdingizmi?');
+
+let natija = `Siz kiritgan ${str} string turiga tegishli
+Siz kiritgan ${bool} boolean turiga tegishli
+Siz kiritgan ${num} number turiga tegishli`
+
+alert(natija);
+
+
+
+
+
+
+
+
+
+
+
+
